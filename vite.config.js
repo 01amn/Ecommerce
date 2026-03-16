74d980e3759340787823b206af71d4479e276548
Aman Mishra <amanm9935905367@gmail.com>
2026-03-16 21:56:57 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/bite/",   // repository name
+  base: "/Ecommerce/",
   plugins: [
     react(),
     tailwindcss(),
